c0888ad86136633d13fda8f5e16aceb0289c683d
Jake Tae <jaesungtae@gmail.com>
2020-09-27 20:36:55 +0900

    chore: reorganize functions into separate file

--- deepzero/core.py
+++ deepzero/core.py
@@ -5,13 +5,40 @@ class Variable:
     def __init__(self, data):
         self.data = data
         self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward_recursive(self):
+        creator_func = self.creator
+        if creator_func is not None:
+            x = creator_func.input
+            x.grad = creator_func.backward(self.grad)
+            x.backward()
+
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        creators = [self.creator]
+        while creators:
+            creator_func = creators.pop()
+            x, y = creator_func.input, creator_func.output
+            x.grad = creator_func.backward(y.grad)
+
+            if x.creator is not None:
+                creators.append(x.creator)
 
 
 class Function:
-    def __call__(self, x):
-        self.x = x
-        y = self.forward(x.data)
-        return Variable(y)
+    def __call__(self, input_):
+        y = self.forward(input_.data)
+        output = Variable(y)
+        output.set_creator(self)
+        self.input = input_
+        self.output = output
+        return output
 
     def forward(self, x):
         raise NotImplementedError
@@ -25,7 +52,7 @@ class Exp(Function):
         return np.exp(x)
 
     def backward(self, grad):
-        return grad * np.exp(self.x.data)
+        return grad * np.exp(self.input.data)
 
 
 class Square(Function):
@@ -33,5 +60,5 @@ class Square(Function):
         return x ** 2
 
     def backward(self, grad):
-        return grad * 2 * self.x.data
+        return grad * 2 * self.input.data
 
---
+++ deepzero/functions.py
@@ -0,0 +1,9 @@
+from deepzero.core import Exp, Square
+
+
+def square(x):
+    return Square()(x)
+
+
+def exp(x):
+    return Exp()(x)
