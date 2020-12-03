883aba20bc4130960d195093cbf2f8e96021d567
Jake Tae <jaesungtae@gmail.com>
2020-12-03 21:14:39 +0900

    feature: setup file for pypi publication

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-YOUR-USERNAME-HERE",
+    version="0.0.1",
+    author="Jake Tae",
+    author_email="jaesungtae@gmail.com",
+    description="A pure Python autograd library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jaketae/pygrad",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
