8fb7a8f5f4bca35b920ce665d1a095acc9b4f7ef
Jake Tae <jaesungtae@gmail.com>
2020-12-16 08:12:40 +0900

    feature: add set_creator, cleanup var names

--- pygrad/core.py
+++ pygrad/core.py
@@ -2,33 +2,37 @@ import numpy as np
 
 
 class Variable:
-    def __init__(self, data, creator=None):
+    def __init__(self, data):
         if data is not None:
             if not isinstance(data, np.ndarray):
                 raise TypeError(f"{type(data)} is not supported.")
 
-        self.grad = None
         self.data = data
-        self.creator = creator
+        self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
 
     def backward(self):
         if self.grad is None:
             self.grad = np.ones_like(self.data)
 
-        creators = [self.creator]
-        while creators:
-            creator_func = creators.pop()
-            x, y = creator_func.input, creator_func.output
-            x.grad = creator_func.backward(y.grad)
+        funcs = [self.creator]
+        while funcs:
+            f = creators.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
 
             if x.creator is not None:
-                creators.append(x.creator)
+                funcs.append(x.creator)
 
 
 class Function:
     def __call__(self, input_):
         y = self.forward(input_.data)
-        output = Variable(as_array(y), creator=self)
+        output = Variable(as_array(y))
+        output.set_creator(self)
         self.input = input_
         self.output = output
         return output
@@ -41,4 +45,6 @@ class Function:
 
 
 def as_array(x):
-    return np.asarray(x) if np.isscalar(x) else x
+    if np.isscalar(x):
+        return np.asarray(x)
+    return x
