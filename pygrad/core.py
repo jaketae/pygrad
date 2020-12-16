29f8d6ad121a8e1e57761d3b4f1e47b078d307c5
Jake Tae <jaesungtae@gmail.com>
2020-12-17 04:49:34 +0900

    fix: accumulate gradients if same variable

--- pygrad/core.py
+++ pygrad/core.py
@@ -6,7 +6,6 @@ class Variable:
         if data is not None:
             if not isinstance(data, np.ndarray):
                 raise TypeError(f"{type(data)} is not supported.")
-
         self.data = data
         self.grad = None
         self.creator = None
@@ -23,7 +22,10 @@ class Variable:
             gys = [output.grad for output in f.outputs]
             gxs = as_tuple(f.backward(*gys))
             for x, gx in zip(f.inputs, gxs):
-                x.grad = gx
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
                 if x.creator is not None:
                     funcs.append(x.creator)
 
