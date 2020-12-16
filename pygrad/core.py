1f6f732415dafc32553c1971124a23a585f9f611
Jake Tae <jaesungtae@gmail.com>
2020-12-17 04:19:49 +0900

    fix: typo instance -> isinstance

--- pygrad/core.py
+++ pygrad/core.py
@@ -21,11 +21,14 @@ class Variable:
         funcs = [self.creator]
         while funcs:
             f = funcs.pop()
-            x, y = f.input, f.output
-            x.grad = f.backward(y.grad)
-
-            if x.creator is not None:
-                funcs.append(x.creator)
+            gys = [output.grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+            for x, gx in zip(f.inputs, gxs):
+                x.grad = gx
+                if x.creator is not None:
+                    funcs.append(x.creator)
 
 
 class Function:
