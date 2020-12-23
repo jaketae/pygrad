a4e6a7f5decded3c05319a25c01fce30248c4a62
Jake Tae <jaesungtae@gmail.com>
2020-12-23 22:06:06 +0900

    feature: populate layer cls

--- pygrad/layers.py
+++ pygrad/layers.py
@@ -1,11 +1,34 @@
-from pygrad.core import Parameter
+import weakref
+
+import pygrad.functions as F
+from pygrad.core import Parameter, as_tuple
 
 
 class Layer:
     def __init__(self):
-        self.params = set()
+        self._params = set()
 
     def __setattr__(self, name, value):
         if isinstance(value, Parameter):
-            self.params.add(name)
+            self._params.add(name)
         super(Layer, self).__setattr__(name, value)
+
+    def __call__(self, *inputs):
+        outputs = as_tuple(self.forward(*inputs))
+        self.inputs = [weakref.ref(x) for x in inputs]
+        self.outputs = [weakref.ref(y) for y in outputs]
+        if len(outputs) > 1:
+            return outputs
+        return outputs[0]
+
+
+    def forward(self, inputs):
+        raise NotImplementedError
+
+    def params(self):
+        for param in self._params:
+            yield self.__dict__[param]
+
+    def clear_grads(self):
+        for param in self._params:
+            param.clear_grad()
