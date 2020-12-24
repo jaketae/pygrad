7948051c9a26a463fd9467a6c99ed56729a60157
Jake Tae <jaesungtae@gmail.com>
2020-12-25 05:57:55 +0900

    feature: add adagrad optimizer

--- pygrad/nn/optim.py
+++ pygrad/nn/optim.py
@@ -35,4 +35,20 @@ class SGD(Optimizer):
         v = self.momentum * self.vs[v_key]
         v -= self.lr * param.grad.data
         param.data += v
-        self.vs[v_key] = v
+
+
+class AdaGrad(Optimizer):
+    def __init__(self, target, lr=1e-2, eps=1e-10):
+        super(AdaGrad, self).__init__(target)
+        self.gs = {}
+        self.lr = lr
+        self.eps = eps
+
+    def ste_one(self, param):
+        g_key = id(param)
+        if g_key not in self.gs:
+            self.gs[g_key] = np.zeros_like(param.data)
+        grad = param.grad.data
+        g = self.gs[g_key]
+        g += grad * grad
+        param.data -= self.lr * grad / (np.sqrt(g) + self.eps)
