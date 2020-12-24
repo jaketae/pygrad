511b974fa8094a4f92f156797d6d3f524ee82f51
Jake Tae <jaesungtae@gmail.com>
2020-12-24 20:34:31 +0900

    feature: sgd w/ momentum

--- pygrad/optimizers.py
+++ pygrad/optimizers.py
@@ -18,10 +18,18 @@ class Optimizer:
 
 
 class SGD(Optimizer):
-    def __init__(self, target, lr=1e-2):
+    def __init__(self, target, lr=1e-2, momentum=0.9):
         super(SGD, self).__init__(target)
+        self.vs = {}
         self.lr = lr
+        self.momentum = momentum
 
-    def step_one(self):
-        param.data -= self.lr * param.grad.data
+    def step_one(self, param):
+        v_key = id(param)
+        if v_key not in self.vs:
+            self.vs[v_key] = np.zeros_like(param.data)
+        v = self.momentum * self.vs[v_key]
+        v -= self.lr * param.grad.data
+        param.data += v
+        self.vs[v_key] = v
 
