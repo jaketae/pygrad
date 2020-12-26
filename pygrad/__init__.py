47a10976bb07d0cff90dadf093ac6ac6b4c6d691
Jake Tae <jaesungtae@gmail.com>
2020-12-27 06:08:40 +0900

    chore: export parameter cls

--- pygrad/__init__.py
+++ pygrad/__init__.py
@@ -2,6 +2,6 @@ import pygrad.data
 import pygrad.exceptions
 import pygrad.functions
 import pygrad.nn
-from pygrad.core import Variable, no_grad, setup_variable
+from pygrad.core import Variable, no_grad, setup_variable, Parameter
 
 setup_variable()
