5ea0c76e04525da1ab789fec1aa20f2a43e5b3d8
Jake Tae <jaesungtae@gmail.com>
2020-12-28 23:00:14 +0900

    chore: add parameter cls to root name space

--- pygrad/__init__.py
+++ pygrad/__init__.py
@@ -2,6 +2,6 @@ import pygrad.data
 import pygrad.exceptions
 import pygrad.functions
 import pygrad.nn
-from pygrad.core import Variable, no_grad, setup_variable, Parameter
+from pygrad.core import Parameter, Variable, as_variable, no_grad, setup_variable
 
 setup_variable()
