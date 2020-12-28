a54e4ff660283a77ddc57e38dd3d8de4ae77506e
Jake Tae <jaesungtae@gmail.com>
2020-12-29 06:30:34 +0900

    version: release 0.1.0

--- pygrad/__init__.py
+++ pygrad/__init__.py
@@ -5,3 +5,5 @@ import pygrad.nn
 from pygrad.core import Parameter, Variable, as_variable, no_grad, setup_variable
 
 setup_variable()
+
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -5,16 +5,19 @@ with open("README.md", "r") as f:
 
 setup(
     name="pygrad",
-    version="0.0.2",
+    version="0.1.0",
     author="Jake Tae",
     author_email="jaesungtae@gmail.com",
     description="A pure Python autograd library",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jaketae/pygrad",
-    packages=find_packages("pygrad", exclude=["tests"]),
+    packages=find_packages(exclude=["docs", "tests"]),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
