532ce558f04cecd590aa7f6072e036c306d67e3f
Jake Tae <jaesungtae@gmail.com>
2020-12-30 08:51:21 +0900

    chore: add utils & functions to root name space

--- pygrad/__init__.py
+++ pygrad/__init__.py
@@ -2,6 +2,7 @@ import pygrad.data
 import pygrad.exceptions
 import pygrad.functions
 import pygrad.nn
-from pygrad.core import Parameter, Variable, as_variable, no_grad
+import pygrad.utils
+from pygrad.core import Function, Parameter, Variable, as_variable, no_grad
 
 __version__ = "0.1.0"
